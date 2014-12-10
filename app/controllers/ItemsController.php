9e0fd49b61d387b33a427ca5d8fe2acdc446d424
viljar <viljar.salu@elion.ee>
2014-12-10 18:17:49 +0200

    added distance query

--- app/controllers/ItemsController.php
+++ app/controllers/ItemsController.php
@@ -24,6 +24,48 @@ class ItemsController extends \BaseController {
 		$d 	= Helper::get_time_ago(strtotime('now'));
 		//return "test". $d;
 
+		/* test */
+		/*$test = DB::select('SELECT *, ((ACOS(SIN(59.436961 * PI() / 180) * SIN(lat * PI() / 180) + COS(59.436961 * PI() / 180) * COS(lat * PI() / 180) * COS((24.753575 - lng) * PI() / 180)) * 180 / PI()) * 60 * 1.1515 * 1.609344) as distance 
+		HAVING distance <= 100 
+		ORDER BY distance ASC');*/
+		
+		$dist 	= 10;
+		$mylat 	= 59.436961;
+		$mylng 	= 24.753575;
+		$lon1 	= $mylng-$dist/abs(cos(radians($mylat))*69);
+		$lon2 	= $mylng+$dist/abs(cos(radians($mylat))*69);
+		$lat1 	= $mylat-($dist/69);
+		$lat2 	= $mylat+($dist/69);
+
+		(59.436961+(10/69))
+
+		SELECT *, 3956 * 2 * ASIN(SQRT( POWER(SIN(($mylat - `lat`) * pi()/180 / 2), 2) +COS($mylat * pi()/180) * COS(`lat` * pi()/180) *POWER(SIN(($mylng - `lng`) * pi()/180 / 2), 2) )) as distance 
+		FROM `vl_addresses`, WHERE `lng` between $lon1 and $lon2 and `lat` between $lat1 and $lat2 
+		having distance < $dist ORDER BY Distance limit 10
+		
+		/* optimeeritud toimiv query */
+		SELECT *, 3956 * 2 * ASIN(SQRT( POWER(SIN((59.436961 - `lat`) * pi()/180 / 2), 2) +COS(59.436961 * pi()/180) * COS(`lat` * pi()/180) *POWER(SIN((24.753575 - `lng`) * pi()/180 / 2), 2) )) as distance 
+		FROM `vl_addresses` WHERE `lng` between (24.753575-10/abs(cos(radians(59.436961))*69)) and ((24.753575+10/abs(cos(radians(59.436961))*69))) and `lat` between (59.436961-(10/69)) and (59.436961+(10/69)) 
+		having distance < 300 Order By Distance Limit 2
+		/* end / optimeeritud toimiv query */
+
+		/* toimiv query */
+		/*SELECT *, 3956 * 2 * ASIN(SQRT( POWER(SIN((59.436961 - abs(`lat`)) * pi()/180 / 2),2) + COS(59.436961 * pi()/180 ) * COS(abs(`lat`) *  pi()/180) * POWER(SIN((24.753575 - `lng`) *  pi()/180 / 2), 2) )) 
+		as distance 
+		FROM `vl_addresses`
+		having distance < 100 
+		ORDER BY distance limit 10;*/
+		/* / toimiv query */
+
+
+
+
+
+
+
+
+		/* end / test */
+
 		$items = Item::with('content','comments','votes','votedusers','labels','assets')->where('public','=',1)->orderBy('created_at','DESC')->get();
 		//return $items;
 
--- app/views/items/show.blade.php
+++ app/views/items/show.blade.php
@@ -8,7 +8,7 @@
             @endforeach
 
             @foreach ($value->addresses as $key => $address)
-                <a href="#" data-toggle="modal" data-target="#google_map" class="pull-right"><span class="glyphicon glyphicon-map-marker"></span> {{ $address->street_address }}</a>
+                <a href="#" data-toggle="modal" data-target="#google_map" class="pull-right" data-lat="{{ $address->lat }}" data-lng="{{ $address->lng }}"><span class="glyphicon glyphicon-map-marker"></span> {{ $address->street_address }}</a>
                 @include('modals.google_map.modal', array('lat'=>$address->lat, 'lng'=>$address->lng) )
                 <!-- <a href="http://maps.googleapis.com/maps/api/directions/json?origin=6.914556,79.973194&destination={{ $address->lat }},{{ $address->lng }}&sensor=false" target="_blank">get distance</a> -->
             @endforeach
@@ -174,94 +174,8 @@
 @endforeach
 </div>
 
-
-
-<div class="row hide">
-  <div class="col-sm-6 col-md-4">
-
-    <div class="thumbnail">
-        <span class="glyphicon glyphicon-fire"></span> plan and price sample
-        <a href="#" class="pull-right"><span class="glyphicon glyphicon-map-marker"></span>location</a>
-
-        <img data-src="holder.js/300x300" alt="...">
-        <div class="caption">
-            <h3>Title <small>4 Nov 2014</small></h3>
-            <p>Content</p>
-        </div>
-
-    <!-- votes -->
-        <div class="votes">
-            <a href="#"><span class="glyphicon glyphicon-thumbs-up"></span>40</a>
-            <a href="#"><span class="glyphicon glyphicon-thumbs-down"></span>10</a>
-        </div>
-    <!-- / votes -->
-
-    <!-- comments -->
-        <div class="comments">
-            <!-- show comments -->
-            <a href="#comments" data-toggle="collapse" data-target="#comments">3 comments <span class="glyphicon glyphicon-chevron-down"></span></a>
-            <div id="comments" class="collapse">
-                <div class="media">
-                    <a class="media-left" href="#">
-                        <img data-src="holder.js/64x64" src="..." alt="...">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Media heading <small>Yesterday 12:45</small></h4>
-                        comment goes here
-                    </div>
-                </div>
-                <div class="media">
-                    <a class="media-left" href="#">
-                        <img data-src="holder.js/64x64" src="..." alt="...">
-                    </a>
-                    <div class="media-body">
-                        <h4 class="media-heading">Media heading <small>Yesterday 22:10</small></h4>
-                        comment goes here
-                    </div>
-                </div>
-            </div>
-
-            <!-- last comment goes here -->
-            <div class="media">
-                <a class="media-left" href="#">
-                    <img data-src="holder.js/64x64" src="..." alt="...">
-                </a>
-                <div class="media-body">
-                    <h4 class="media-heading">Media heading <small>Today 09:45</small></h4>
-                    last comment goes here
-                </div>
-            </div>
-
-            <!-- add comments -->
-            <div class="comment__add">
-                <a href="#comment_form" class="btn btn-primary btn-sm center-block" data-toggle="collapse" data-target="#comment_form">{{ Lang::get('text.add_comment') }}</a>
-                <div id="comment_form" class="media collapse">
-                    <a class="media-left" href="#">
-                        <img data-src="holder.js/64x64" src="..." alt="user profile picture">
-                    </a>
-                    <div class="media-body">
-                        {{ Form::open(array('url'=>'comments/add', 'role'=>'form', 'id'=>'add')) }}
-                            {{ Form::hidden('item_id', 1, array('id'=>'item_id')) }}
-                            {{ Form::label('comment_title', Lang::get('text.add_comment_title')) }}
-                            {{ Form::text('comment_title', null, array('class'=>'form-control', 'placeholder'=>Lang::get('text.comment_title'),'id'=>'comment_title')) }}
-                            {{ Form::label('comment', Lang::get('text.comment')) }}
-                            {{ Form::textarea('comment', null, array('class'=>'form-control', 'placeholder'=>Lang::get('text.comment'),'id'=>'comment')) }}
-                            {{ Form::submit(Lang::get('text.add_comment'), array('class'=>'btn btn-sm btn-primary'))}}
-                            {{ Form::button(Lang::get('text.cancel'), array('class'=>'btn btn-sm btn-default', 'data-toggle'=>'collapse', 'data-target'=>'#comment_form'))}}
-                        {{ Form::close() }}
-                    </div>
-                </div>
-            </div>
-        </div>
-    <!-- / comments -->
-    </div>
-
-  </div>
-</div>
-
 <style type="text/css">
     .votes a > span:last-child {
         margin-left: 10px;
     }
 </style>
-<script src="http://imsky.github.io/holder/holder.js" />
